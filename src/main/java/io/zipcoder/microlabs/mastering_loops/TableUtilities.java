c91c712128e1f1135d5d035e3207e9855d5e4e68
Zachary <zachary@Zipcoders-MacBook-Pro-12.local>
2023-03-06 20:14:28 -0500

    tried to get getMultiplicationTable figured out... close but \n seems to not be printing

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,17 +1,30 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.sql.SQLOutput;
+
 public class TableUtilities {
+        int tableParams = 0;
     public static String getSmallMultiplicationTable() {
 
         return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return getMultiplicationTable(5);
+
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
+            int a = 1;
+            int b = 1;
+        for (a = 1; a <= tableSize; a++){
 
-        return getMultiplicationTable(5);
+            for (b = 1; b <= tableSize; b++){
+                System.out.print(a*b + " " + "|" + " ");
+            }
+            System.out.println();
+
+        }
+        return getMultiplicationTable(0);
     }
 }
